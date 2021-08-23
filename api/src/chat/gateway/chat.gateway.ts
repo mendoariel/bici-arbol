b1d02d35d44ad1b0a5bd378d38e66d4e6cab5693
albertoarielarce <aarce@alquilando.com>
2021-08-23 13:08:27 -0300

    video-07: add a gateway conection

---
+++ api/src/chat/gateway/chat.gateway.ts
@@ -0,0 +1,44 @@
+import { UnauthorizedException } from '@nestjs/common';
+import { OnGatewayConnection, OnGatewayDisconnect, SubscribeMessage, WebSocketGateway, WebSocketServer } from '@nestjs/websockets';
+import { Socket, Server  } from 'socket.io';
+import { AuthService } from 'src/auth/service/auth.service';
+import { UserI } from 'src/user/model/user.interface';
+import { UserService } from 'src/user/service/user-service/user.service';
+
+@WebSocketGateway({cors: { origin: ['https://hoppscotch.io', 'http://localhost:3000', 'http://localhost:4200']}})
+export class ChatGateway implements OnGatewayConnection, OnGatewayDisconnect {
+
+  @WebSocketServer()
+  server: Server;
+  
+
+  title: string[] = [];
+
+  constructor(private authService: AuthService, private userService: UserService) {}
+
+  async handleConnection(socket: Socket) {
+    try {
+       const decodedToken = await this.authService.verifyJwt(socket.handshake.headers.authorization );
+       const user: UserI = await this.userService.getOne(decodedToken.user.id);
+       
+       if(!user) {
+         this.disconnect(socket);
+       } else {
+         this.title.push('Value ' + Math.random( ).toString());
+         this.server.emit('message', this.title);
+       }
+      
+    } catch {
+      this.disconnect(socket);
+    }
+  }
+  
+  handleDisconnect(socket: Socket) {
+    socket.disconnect();
+  }
+
+  private disconnect(socket: Socket) {
+    socket.emit('Error', new UnauthorizedException());
+    socket.disconnect();
+  }
+}
