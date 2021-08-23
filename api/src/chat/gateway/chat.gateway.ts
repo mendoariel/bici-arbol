cb17210b9559e65e660b9100f98e5114af7080ec
albertoarielarce <aarce@alquilando.com>
2021-08-23 17:55:58 -0300

    create a room entity

--- api/src/chat/gateway/chat.gateway.ts
+++ api/src/chat/gateway/chat.gateway.ts
@@ -4,17 +4,16 @@ import { Socket, Server  } from 'socket.io';
 import { AuthService } from 'src/auth/service/auth.service';
 import { UserI } from 'src/user/model/user.interface';
 import { UserService } from 'src/user/service/user-service/user.service';
+import { RoomI } from '../model/room.interface';
+import { RoomService } from '../service/room-service/room/room.service';
 
 @WebSocketGateway({cors: { origin: ['https://hoppscotch.io', 'http://localhost:3000', 'http://localhost:4200']}})
 export class ChatGateway implements OnGatewayConnection, OnGatewayDisconnect {
 
   @WebSocketServer()
   server: Server;
-  
 
-  title: string[] = [];
-
-  constructor(private authService: AuthService, private userService: UserService) {}
+  constructor(private authService: AuthService, private userService: UserService, private roomService: RoomService) {}
 
   async handleConnection(socket: Socket) {
     try {
@@ -24,8 +23,11 @@ export class ChatGateway implements OnGatewayConnection, OnGatewayDisconnect {
        if(!user) {
          this.disconnect(socket);
        } else {
-         this.title.push('Value ' + Math.random( ).toString());
-         this.server.emit('message', this.title);
+         socket.data.user = user;
+         const rooms = await this.roomService.getRoomsForUser(user.id, {page: 1, limit: 10}); 
+
+         //  Only emit rooms to the specific connected client
+         return this.server.to(socket.id).emit('rooms', rooms);
        }
       
     } catch {
@@ -40,5 +42,11 @@ export class ChatGateway implements OnGatewayConnection, OnGatewayDisconnect {
   private disconnect(socket: Socket) {
     socket.emit('Error', new UnauthorizedException());
     socket.disconnect();
+  }
+
+  @SubscribeMessage('createRoom')
+  async onCreateRoom(socket: Socket, room: RoomI):Promise<RoomI> {
+    console.log(socket.data.user);
+    return this.roomService.createRoom(room, socket.data.user)
   }
 }
