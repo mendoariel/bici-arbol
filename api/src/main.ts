3f8967bd23d7fce5ec7d08310027502328743473
mendoariel <mendoariel@hotmail.com>
2021-10-27 12:35:03 -0300

    fix some thing

--- api/src/main.ts
+++ api/src/main.ts
@@ -2,10 +2,15 @@ import { ValidationPipe } from '@nestjs/common';
 import { NestFactory } from '@nestjs/core';
 import { AppModule } from './app.module';
 
+import * as express from 'express';
+import { join } from 'path';
+
 async function bootstrap() {
   const app = await NestFactory.create(AppModule);
   app.setGlobalPrefix('api');
   app.useGlobalPipes(new ValidationPipe());
+  app.use(express.static(join(process.cwd(), '../client/dist/')));
+
   await app.listen(3000);
 }
 bootstrap();
