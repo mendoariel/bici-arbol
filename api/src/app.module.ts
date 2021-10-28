4e35ba154b71873f510460b64e54f182138eca62
mendoariel <mendoariel@hotmail.com>
2021-10-28 15:46:10 -0300

    fix same change

--- api/src/app.module.ts
+++ api/src/app.module.ts
@@ -7,17 +7,22 @@ import { UserModule } from './user/user.module';
 import { AuthModule } from './auth/auth.module';
 import { AuthMiddleware } from './middleware/auth.middleware';
 import { ChatModule } from './chat/chat.module';
+import { ServeStaticModule } from '@nestjs/serve-static';
+import { join } from 'path';
 
 @Module({
   imports: [
+    ServeStaticModule.forRoot({
+      rootPath: join(__dirname, './', '../frontend/'),
+    }),
     ConfigModule.forRoot({isGlobal: true}),
     TypeOrmModule.forRoot({
       type: 'postgres',
       host: process.env.host,
-      port: 5432,
-      username: 'mac',
-      password: '1234',
-      database: 'db',
+      port: parseInt(process.env.port),
+      username: process.env.username,
+      password: process.env.password,
+      database: process.env.db,
       autoLoadEntities: true,
       synchronize: true
     }),
