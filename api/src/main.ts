a7f1583bf87ebb72022e9ba75536d791e1299303
mendoariel <mendoariel@hotmail.com>
2021-10-29 09:50:26 -0300

    delete express from main

--- api/src/main.ts
+++ api/src/main.ts
@@ -2,15 +2,10 @@ import { ValidationPipe } from '@nestjs/common';
 import { NestFactory } from '@nestjs/core';
 import { AppModule } from './app.module';
 
-import * as express from 'express';
-import path, { join } from 'path';
-
 async function bootstrap() {
   const app = await NestFactory.create(AppModule);
   app.setGlobalPrefix('api');
   app.useGlobalPipes(new ValidationPipe());
-
-  //app.use(express.static(path.join('../../frontend/dist/frontend/')))
 
   await app.listen(3000);
 }
